0c4025045a92e3e783b82f909e76c8fa501ec0c8
Aitor Lucas <aitor.eler.lucas@gmail.com>
2023-04-20 18:05:18 -0300

    add first player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private float speed = 7f;
+
+    private void Update() {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+
+    }
+
+}
