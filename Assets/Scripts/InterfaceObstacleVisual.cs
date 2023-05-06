e763bb41439d7629c0c206740cec25d4de00538d
Aitor Lucas <aitor.eler.lucas@gmail.com>
2023-05-06 00:46:29 -0300

    create some obstacles and their logics

---
+++ Assets/Scripts/InterfaceObstacleVisual.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface InterfaceObstacleVisual {
+    // [SerializeField] private Obstacle obstacle;
+    // public Transform GetKitchenObjectFollowTransform();
+    // public void SetKitchenObject(KitchenObject kitchenObject);
+    // public KitchenObject GetKitchenObject();
+    // public void ClearKitcheObject();
+    // public bool HasKitchenObject();
+}
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,7 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour {
 
+    float life;
+
+    public static event EventHandler OnObstacleDestroy;
+
+    private void OnDestroy() {
+        OnObstacleDestroy?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void InflictDamage(float damage) {
+        life -= damage;
+
+        if (life <= 0) {
+            Destroy(this);
+        }
+    }
 }
